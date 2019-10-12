62141741260a2b78ac657465ce84345a63706cea
upkar-singh <uupkarssingh@gmail.com>
2019-10-11 20:49:12 -0400

    fixed up navigation

--- app/src/main/java/com/example/singhnicershop/MenuActivity.java
+++ app/src/main/java/com/example/singhnicershop/MenuActivity.java
@@ -1,8 +1,10 @@
 package com.example.singhnicershop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -28,6 +30,11 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        ActionBar ab = getSupportActionBar();
+
+        ab.setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
